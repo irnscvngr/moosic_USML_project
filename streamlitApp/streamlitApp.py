ecbb73b2fda1493d04f92218fa15418e2630035d
irnscvngr <161852309+irnscvngr@users.noreply.github.com>
2024-04-27 21:55:19 +0200

    Update streamlitApp.py
    test streamlitApp/ as file location for logo

--- streamlitApp/streamlitApp.py
+++ streamlitApp/streamlitApp.py
@@ -19,7 +19,7 @@ import plotly.graph_objects as go
 # =============================================================================
 # PAGE TITLE AND IMAGE
 # =============================================================================
-st.image('https://github.com/irnscvngr/moosic_USML_project/blob/main/streamlitApp/Moose_Logo.png')
+st.image('streamlitApp/Moose_Logo.png')
 
 # Draw a title and some text to the app:
 '''
